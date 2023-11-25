4937a18099b8a2ea398d84d7d4164627b4e99a1b
maheshkumawat274 <maheshkumar006376@gmail.com>
2023-11-26 00:21:40 +0530

    Multiple classes and querySelector

---
+++ multipleEvents.js
@@ -0,0 +1,11 @@
+var h  = document.querySelectorAll("h1");
+h.forEach(function(e){
+ console.log(e);
+});
+
+var e = document.getElementsByClassName("p1")[0];
+  e.style.backgroundColor="pink";
+  var e = document.getElementsByClassName("p1")[1];
+  e.style.backgroundColor="yellow";
+  var e = document.getElementsByClassName("p1")[2];
+  e.style.backgroundColor="blue";
